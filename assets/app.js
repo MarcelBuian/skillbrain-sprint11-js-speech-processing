be137c0d04d6b50facbb8d7dfc9aaa274ac202fd
IoanaDodi <ioanadodita1998@gmail.com>
2024-06-05 21:01:48 +0300

    am adaugat jokes

--- assets/app.js
+++ assets/app.js
@@ -1,44 +1,68 @@
 const applyToastrOptions = () => {
     toastr.options = {
-        "closeButton": true,
-        "debug": false,
-        "newestOnTop": false,
-        "progressBar": true,
-        "positionClass": "toast-top-right",
-        "preventDuplicates": true,
-        "onclick": null,
-        "showDuration": "1000",
-        "hideDuration": "1000",
-        "timeOut": "5000",
-        "extendedTimeOut": "1000",
-        "showEasing": "swing",
-        "hideEasing": "linear",
-        "showMethod": "fadeIn",
-        "hideMethod": "fadeOut"
-    }
-}
+        closeButton: true,
+        debug: false,
+        newestOnTop: false,
+        progressBar: true,
+        positionClass: "toast-top-right",
+        preventDuplicates: true,
+        onclick: null,
+        showDuration: "1000",
+        hideDuration: "1000",
+        timeOut: "5000",
+        extendedTimeOut: "1000",
+        showEasing: "swing",
+        hideEasing: "linear",
+        showMethod: "fadeIn",
+        hideMethod: "fadeOut",
+    };
+};
 
 const speak = (message) => {
     VoiceRSS.speech({
-        key: '52bfbd75e1c24955831329926a53c5ed',
+        key: "52bfbd75e1c24955831329926a53c5ed",
         src: message,
-        hl: 'en-us',
-        v: 'Linda',
+        hl: "en-us",
+        v: "Linda",
         r: 0,
-        c: 'mp3',
-        f: '44khz_16bit_stereo',
-        ssml: false
+        c: "mp3",
+        f: "44khz_16bit_stereo",
+        ssml: false,
     });
-}
+};
 
 const jokeClickListener = () => {
-    toastr.info('Joke is coming...');
+    // toastr.info("Joke is coming...");
     const joke = "Hello World!";
-    speak(joke);
-}
+    // speak(joke);
 
-document.addEventListener('DOMContentLoaded', () => {
+    const apiUrl = "https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,racist,sexist,explicit&type=twopart&amount=2";
+    const requestOptions = {};
+
+    fetch(apiUrl, requestOptions)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error("Network response was not ok");
+            }
+            return response.json();
+        })
+        .then((data) => {
+            // console.log(data);
+            const jokeQuestion = data.jokes[0].setup;
+            // console.log(jokeQuestion);
+            const jokeAnswer = data.jokes[0].delivery;
+            // console.log(jokeAnswer);
+            toastr["success"](jokeAnswer, jokeQuestion);
+            // document.getElementById
+        })
+        .catch((error) => {
+            console.error("Error:", error);
+            toastr.error("Error: " + error);
+        });
+};
+
+document.addEventListener("DOMContentLoaded", () => {
     applyToastrOptions();
-    const button = document.getElementById('joke-button');
-    button.addEventListener('click', jokeClickListener);
+    const button = document.getElementById("joke-button");
+    button.addEventListener("click", jokeClickListener);
 });
